ae4cbf6b5022cd15046b60ee58b98e7ff5c09fcc
pavinich <yanahamineshko@gmail.com>
2024-07-14 00:28:15 +0300

    Домашнее задание 4
    Создала список покупок

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.getElementById('input');
+const itemsContainer = document.querySelector('.items');
+
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const itemText = sendInput.value;
+
+        const newItem = document.createElement('div');
+        newItem.textContent = itemText;
+
+        if (itemText != '') {
+            itemsContainer.append(newItem);
+        }
+
+        newItem.addEventListener('click', function() {
+            newItem.classList.toggle('done');
+        });
+        
+        sendInput.value = '';
+    }
+});
+
 
 /* Пустые элементы не должны добавляться */
 
